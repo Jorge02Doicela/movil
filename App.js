fae67d1ecd40753b40ee19b88a6c1031baaf3a92
JorgeDoicela <jorge.doicela@istpet.edu.ec>
2025-05-08 10:59:02 -0400

    mi segundo commit

--- App.js
+++ App.js
@@ -1,11 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! algo </Text>
-      <StatusBar style="auto" />
+      <Text>Open up App.js to start working on your app! </Text>
     </View>
   );
 }
